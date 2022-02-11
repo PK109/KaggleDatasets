85e3358a6bc3d3d4ec8ad30a3d1e398f0e59a6b8
PK109 <kuta.przemyslaw@gmail.com>
2022-02-11 06:56:05 +0100

    Added automation for downloading, extracting and importing data for Titanic.

--- ML_scripts/titanic_survivors.py
+++ ML_scripts/titanic_survivors.py
@@ -1,13 +1,37 @@
 import pandas as pd
 import numpy as np
 import seaborn as sns
+from zipfile import ZipFile
+import os
+
+# data download & extraction
+path_download = os.path.join(os.path.dirname(os.path.dirname(os.path.realpath(__file__))), 'helpers', 'competition_downloader.py')
+path_zip = "../competitions/titanic.zip"
+path_out = "../competitions/titanic"
+
+if not os.path.exists(path_zip):
+    os.system(f'python \"{path_download}\" titanic')
+    while not os.path.exists(path_zip):
+        pass
+
+if not os.path.exists(path_out):
+    with ZipFile(path_zip, 'r') as zip_ref:
+        zip_ref.extractall(path_out)
+
+all_files = os.listdir(path_out)
+print(all_files)
 
 # data import
-path = "../competitions/titanic.zip"
-df = pd.read_csv(path, index_col='PassengerId')
 
-# print(df.head())
-# print(df.describe())
-print(df.info())
+y_test = pd.read_csv(os.path.join(path_out,all_files[0]), index_col='PassengerId')
+X_test = pd.read_csv(os.path.join(path_out,all_files[1]), index_col='PassengerId')
+X_train = pd.read_csv(os.path.join(path_out,all_files[2]), index_col='PassengerId')
+y_train = pd.read_csv(os.path.join(path_out,all_files[2]), index_col='PassengerId', usecols=['PassengerId','Survived'])
+
+X_train.drop(columns='Survived', inplace=True)
+
+print(X_train.head())
+print(y_train.head())
 
 # data analysis
+
